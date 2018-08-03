705568ef1401f9d02a817b43f50abe7ead7ace44
Andrew Garner <andrewgarner101@gmail.com>
2018-08-03 11:57:56 -0700

    Added empty arduino sketch

---
+++ Arduino/button-sketch/button-sketch.ino
@@ -0,0 +1,20 @@
+/**
+   Sketch that shows usage of a button to turn on an LED
+
+   Using:
+   1. Arduino Uno R3
+   2. LED
+   3. Button
+
+   Andrew Garner
+   August 3, 2018
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
