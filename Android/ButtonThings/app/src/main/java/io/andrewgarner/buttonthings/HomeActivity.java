639612d527f086a5f39b3fe9757cc571ea6c02e3
Andrew Garner <andrewgarner101@gmail.com>
2018-08-03 16:18:53 -0700

    Button & LED implementation. Turns on LED when button is pressed.

--- Android/ButtonThings/app/src/main/java/io/andrewgarner/buttonthings/HomeActivity.java
+++ Android/ButtonThings/app/src/main/java/io/andrewgarner/buttonthings/HomeActivity.java
@@ -19,8 +19,10 @@ public class HomeActivity extends Activity {
     private static final String LOG_TAG = HomeActivity.class.getSimpleName();
 
     public static final String BUTTON_PIN = "BCM2";
+    public static final String LED_PIN = "BCM3";
 
     private Gpio mButtonGpio;
+    private Gpio mLedGpio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +35,31 @@ public class HomeActivity extends Activity {
         try {
             mButtonGpio = manager.openGpio(BUTTON_PIN);
             mButtonGpio.setDirection(Gpio.DIRECTION_IN);
-            mButtonGpio.setEdgeTriggerType(Gpio.EDGE_FALLING);
+            mButtonGpio.setEdgeTriggerType(Gpio.EDGE_BOTH);
             mButtonGpio.registerGpioCallback(mCallback);
         } catch (IOException e) {
             Log.e(LOG_TAG, "Failed to open Button GPIO", e);
+        }
+
+        try {
+            mLedGpio = manager.openGpio(LED_PIN);
+            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Failed to open LED GPIO", e);
+        }
+    }
+
+    /**
+     * On button state change, changes LED state.
+     * Button pressed down -> LED ON
+     * Button neutral -> LED OFF
+     */
+    private void onButtonStateChange(final boolean isPressedDown) {
+        try {
+            mLedGpio.setValue(isPressedDown);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Failed to set value LED GPIO", e);
         }
     }
 
@@ -52,12 +75,31 @@ public class HomeActivity extends Activity {
                 Log.e(LOG_TAG, "Failed to close Button GPIO", e);
             }
         }
+
+        if (mLedGpio != null) {
+            try {
+                mLedGpio.close();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Failed to close LED GPIO", e);
+            }
+        }
     }
 
     private GpioCallback mCallback = new GpioCallback() {
         @Override
         public boolean onGpioEdge(final Gpio gpio) {
-            Log.d(LOG_TAG, "Button Pressed! " + System.currentTimeMillis());
+            try {
+                boolean buttonValue = gpio.getValue();
+                if (buttonValue) {
+                    Log.d(LOG_TAG, "Button Released");
+                } else {
+                    Log.d(LOG_TAG, "Button Pressed");
+                }
+                onButtonStateChange(!buttonValue);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Failed to get value Button GPIO", e);
+            }
+
             //Returns true to continue getting updates.
             return true;
         }
